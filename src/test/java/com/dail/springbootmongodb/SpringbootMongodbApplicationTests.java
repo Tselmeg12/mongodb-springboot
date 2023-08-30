ea5cf79c4ee8c4fc102b894b2482b39b01c6b38a
Tselmeg12 <dev.celmeg@gmail.com>
2023-08-29 23:40:41 -0700

    first commit

---
+++ src/test/java/com/dail/springbootmongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dail.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
